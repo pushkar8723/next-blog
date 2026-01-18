98e529e023a314c27240c47112ea54190fe1cb51
Pushkar Anand <pushkar8723@gmail.com>
2026-01-18 16:44:17 +0530

    Updated Work Ex Page

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,31 @@
+import Link from 'next/link';
 import { withBasePath } from './lib/base-path';
 
 export function useMDXComponents(components: Record<string, any> = {}) {
     return {
         ...components,
+        a: (props: any) => {
+            const href =
+                props.href && props.href.startsWith('/')
+                    ? withBasePath(props.href)
+                    : props.href;
+
+            // External links
+            if (href?.startsWith('http')) {
+                return (
+                    // eslint-disable-next-line jsx-a11y/anchor-has-content
+                    <a
+                        {...props}
+                        href={href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    />
+                );
+            }
+
+            // Internal links
+            return <Link {...props} href={href} />;
+        },
         img: (props: any) => {
             const src =
                 props.src && props.src.startsWith('/')
